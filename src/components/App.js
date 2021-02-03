a1f9b1e091092678283b37b7e58f4ce1d50f219f
jeffrey-huang-recaura <jeffreyhuang@recaura.com>
2021-02-03 22:19:52 +1100

    add and configure redux-persist

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,20 @@ import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from '../navigation/AppNavigator';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import { persistStore } from 'redux-persist';
+
+let persistor = persistStore(store);
 
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <StatusBar barStyle="auto" />
-        <AppNavigator />
-      </NavigationContainer>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <StatusBar barStyle="auto" />
+          <AppNavigator />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import rootReducer from './rootReducer';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 // import { offline } from '@redux-offline/redux-offline';
 // import offlineConfig from '@redux-offline/redux-offline/lib/defaults';
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage
+};
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistReducer(persistConfig, rootReducer),
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+    }
+  }),
   devTools: process.env.NODE_ENV !== 'production',
   enhancers: (defaultEnhancers) => [...defaultEnhancers]
-  // offline(offlineConfig)
 });
 
 export default store;

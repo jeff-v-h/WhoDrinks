696a15b3ff5f504ad8394425573063962f15107f
jeffrey-huang-recaura <jeffreyhuang@recaura.com>
2021-02-07 14:48:57 +1100

    update deck list screen

--- src/components/decks/DeckListScreen.js
+++ src/components/decks/DeckListScreen.js
@@ -1,72 +1,22 @@
 import * as React from 'react';
-import { View, SafeAreaView, FlatList, Alert, Text } from 'react-native';
+import { View, SafeAreaView, FlatList, Text } from 'react-native';
 import styles from '../../styles/styles';
 import deckStyles from '../../styles/deckStyles';
 import ListLinkRow from '../common/ListLinkRow';
-import standardDeck from '../../utils/decks/standard-deck';
-import asianDeck from '../../utils/decks/asian-deck';
-import StorageService from '../../services/storageService';
 import IconButton from '../common/IconButton';
-import { ERROR_TITLE } from '../../utils/constants';
+import { selectDeck } from './decksSlice';
+import { connect } from 'react-redux';
+
+const mapState = (state) => ({
+  decks: state.decks
+});
+
+const mapDispatch = { selectDeck };
 
 class DeckListScreen extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      decks: [],
-      selectedDeckId: props.route.params?.selectedDeckId ?? ''
-    };
-  }
-
-  componentDidMount() {
-    this.loadDecks();
-  }
-
-  componentDidUpdate() {
-    const { navigation, route } = this.props;
-    if (route.params?.reloadDeckList) {
-      this.loadDecks();
-      navigation.setParams({ reloadDeckList: false });
-    }
-  }
-
-  loadDecks = async () => {
-    try {
-      const deckList = await StorageService.getDeckList();
-
-      if (!deckList || deckList.length === 0) {
-        const newDecks = [standardDeck, asianDeck];
-        await this.saveInitialDecks(newDecks);
-        this.setState({ decks: newDecks, selectedDeckId: newDecks[0].id });
-        return;
-      }
-
-      this.setState({ decks: deckList });
-    } catch (e) {
-      Alert.alert(ERROR_TITLE, e.message);
-    }
-  };
-
-  saveInitialDecks = async (decks) => {
-    try {
-      await StorageService.saveNewDecks(decks);
-    } catch (e) {
-      Alert.alert(ERROR_TITLE, e.message);
-    }
-  };
-
   selectDeck = async (id) => {
-    if (this.state.selectedDeckId != id) {
-      await StorageService.saveSelectedDeckId(id);
-      this.setState({ selectedDeckId: id });
-
-      const selectedDeck = this.state.decks.find((d) => d.id === id);
-      this.props.navigation.setParams({
-        reloadSelection: true,
-        selectedDeckId: id,
-        selectedDeckName: selectedDeck.name
-      });
+    if (this.props.decks.selectedId != id) {
+      this.props.selectDeck(id);
     }
   };
 
@@ -77,19 +27,30 @@ class DeckListScreen extends React.Component {
   };
 
   render() {
-    const { decks, selectedDeckId } = this.state;
+    const { decks } = this.props;
 
     return (
       <SafeAreaView style={styles.container}>
         <View style={styles.list}>
           <FlatList
-            data={decks}
-            ListHeaderComponent={<Text style={deckStyles.currentlySelectedHeading}>Selected</Text>}
+            data={decks.list}
+            ListHeaderComponent={
+              <Text style={deckStyles.currentlySelectedHeading}>Selected</Text>
+            }
             ListHeaderComponentStyle={deckStyles.deckListHeader}
             renderItem={({ item }) => (
-              <ListLinkRow onPress={() => this.selectDeck(item.id)} text={item.name} viewStyle={deckStyles.listRow}>
+              <ListLinkRow
+                onPress={() => this.selectDeck(item.id)}
+                text={item.name}
+                viewStyle={deckStyles.listRow}
+              >
                 <View style={deckStyles.selectedCol}>
-                  {item.id === selectedDeckId && <IconButton iconName="check" buttonStyle={deckStyles.selectedIcon} />}
+                  {item.id === decks.selectedId && (
+                    <IconButton
+                      iconName="check"
+                      buttonStyle={deckStyles.selectedIcon}
+                    />
+                  )}
                 </View>
                 <Text style={styles.itemText} numberOfLines={1}>
                   {item.name}
@@ -115,4 +76,4 @@ class DeckListScreen extends React.Component {
   }
 }
 
-export default DeckListScreen;
+export default connect(mapState, mapDispatch)(DeckListScreen);
